774576b556f53d959f981df7edbf190f17e8c95f
Insik Cho <insik.cho@alaskaair.com>
2015-11-04 13:08:44 -0800

    Remove unnecessary files

--- app.js
+++
@@ -1,7 +0,0 @@
-/*global $timer, console*/
-
-var timer1 = window.$timer('Timer1', 5, 40, "debug"),
-    timer2 = window.$timer('Timer2', 10, 20, "debug");
-
-timer1.startTimer();
-timer2.startTimer();
