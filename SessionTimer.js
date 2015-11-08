f1b8490b4de9017c827707b5d0c8c99567d6bfaf
iminsik <iminsik@gmail.com>
2015-11-08 12:47:18 -0800

    Refactor SessionTimer.js

--- SessionTimer.js
+++ SessionTimer.js
@@ -4,13 +4,26 @@
 
 (function (global) {
 	'use strict';
-	var sessionTimer = function (warningSecs, expiringSecs) {
-		return new sessionTimer.factory(warningSecs, expiringSecs);
-	};
-	sessionTimer.prototype = {
-	// ********************************************
+	var sessionTimer, prototype;
+	prototype = {
+		// ********************************************
+		// define error constants
+		// ********************************************
+		ERRORS: {
+			POSITIVESESSIONTIMEOUT:
+				'Session timeout must be a positive number.',
+			NONZEROTIMEOUT:
+				'Session timeout can\'t be zero or negative.',
+			POSITIVEWARNINGTIME:
+				'Session timeout warning must be a positive number.',
+			NONWARNINGTIME:
+				'Session timeout warning can\'t be zero or negative.',
+			WARNINGSMALLERTHANTIMEOUT:
+				'Session timeout warning must be smaller than Session timeout.'
+		},
+		// ********************************************
 		// determine how much time elapsed in msec.
-	// ********************************************
+		// ********************************************
 		elapsedInMsec: function () {
 			// convert both dates to milliseconds
 			try {
@@ -25,30 +38,30 @@
 				return 0;
 			}
 		},
-	// ********************************************
+		// ********************************************
 		// check if time has passed.
-	// ********************************************
+		// ********************************************
 		isExpired: function () {
 			return this.elapsedInMsec() > this.sessionTimeoutInMsec;
 		},
-	// ********************************************
+		// ********************************************
 		// check if warning time has come.
-	// ********************************************
+		// ********************************************
 		isWarningThresholdReached: function () {
 			return this.elapsedInMsec() >= this.sessionTimeoutWarningHappensInMsec;
 		},
-	// ********************************************
+		// ********************************************
 		// return time left until expiring.
-	// ********************************************
+		// ********************************************
 		timeLeft: function () {
 			return Math.floor(Math.max(0,
 					 (this.sessionTimeoutInMsec
 					- this.elapsedInMsec()))
 				/ 1000);
 		},
-	// ********************************************
+		// ********************************************
 		// check if time settings are all valid.
-	// ********************************************
+		// ********************************************
 		isSettingsValid: function () {
 			var i = 0, len = 0;
 			for (i = 0, len = this.validSettingFunctions.length; i < len; i = i + 1) {
@@ -58,9 +71,9 @@
 			}
 			return true;
 		},
-	// ********************************************
+		// ********************************************
 		// reset/restart Timer configuration
-	// ********************************************
+		// ********************************************
 		reset: function () {
 			if (this.isResettingSession) { return; }
 
@@ -70,75 +83,92 @@
 			this.sessionHasTimedout = false;
 			this.isResettingSession = false;
 		},
-	// ********************************************
+		// ********************************************
 		// return total percent complete,
-	// ie., ratio of 'elapsed time' to 'time session is active'
+		// ie., ratio of 'elapsed time' to 'time session is active'
 		// UI will want to respond to this countdown
-	// ********************************************
+		// ********************************************
 		totalCountdownPercentComplete: function () {
 			return Math.floor(100 *
 							 (this.elapsedInMsec()
 								/ this.sessionTimeoutInMsec));
 		},
-	// ********************************************
+		// ********************************************
 		// return warning percent complete,
 		// ie., ratio of 'elapsed time' to 'time to wait before warning'
 		// UI may want to respond to this countdown
-	// ********************************************
+		// ********************************************
 		warningCountdownPercentComplete: function () {
 			return Math.floor(100 * ((this.elapsedInMsec()
 								 - this.sessionTimeoutWarningHappensInMsec)
 				/ (this.sessionTimeoutInMsec
 					 - this.sessionTimeoutWarningHappensInMsec)));
+		},
+		assignProperties: function (warningSecs, expiringSecs) {
+			this.count = 1;
+			this.pollTimeInMsec = 50;	// 0.05 sec
+			this.sessionTimeoutInMsec = expiringSecs * 1000;
+			this.sessionTimeoutWarningHappensInMsec = warningSecs * 1000;
+			this.countdownStarted = null;
+			this.sessionHasTimedout = false;
+			this.isResettingSession = false;
 		}
 	};
 
-	// ********************************************
-	// define error constants
-	// ********************************************
-	sessionTimer.prototype.ERRORS = {
-		POSITIVESESSIONTIMEOUT: 'Session timeout must be a positive number.',
-		NONZEROTIMEOUT: 'Session timeout can\'t be zero or negative.',
-		POSITIVEWARNINGTIME: 'Session timeout warning must be a positive number.',
-		NONWARNINGTIME: 'Session timeout warning can\'t be zero or negative.',
-		WARNINGSMALLERTHANTIMEOUT: 'Session timeout warning must be smaller than Session timeout.'
-	};
 	// ********************************************
 	// define error validation functions and msgs
 	// ********************************************
-	sessionTimer.prototype.validSettingFunctions = [
+	prototype.validSettingFunctions = [
 		{
-			isValid: function () { return typeof this.sessionTimeoutInMsec !== 'number'; },
-			msg: function () { return this.ERRORS.POSITIVESESSIONTIMEOUT; }
+			isValid: function () {
+				return typeof this.sessionTimeoutInMsec
+									!== 'number';
+			},
+			msg: function () {
+				return this.ERRORS.POSITIVESESSIONTIMEOUT;
+			}
 		},
 		{
-			isValid: function () { return this.sessionTimeoutInMsec <= 0; },
-			msg: function () { return this.ERRORS.NONZEROTIMEOUT; }
+			isValid: function () {
+				return this.sessionTimeoutInMsec <= 0;
+			},
+			msg: function () {
+				return this.ERRORS.NONZEROTIMEOUT;
+			}
 		},
 		{
-			isValid: function () { return typeof this.sessionTimeoutWarningHappensInMsec !== 'number'; },
-			msg: function () { return this.ERRORS.POSITIVEWARNINGTIME; }
+			isValid: function () {
+				return typeof this.sessionTimeoutWarningHappensInMsec
+									!== 'number';
+			},
+			msg: function () {
+				return this.ERRORS.POSITIVEWARNINGTIME;
+			}
 		},
 		{
-			isValid: function () { return this.sessionTimeoutWarningHappensInMsec <= 0; },
-			msg: function () { return this.ERRORS.NONWARNINGTIME; }
+			isValid: function () {
+				return this.sessionTimeoutWarningHappensInMsec <= 0;
+			},
+			msg: function () {
+				return this.ERRORS.NONWARNINGTIME;
+			}
 		},
 		{
-			isValid: function () { return this.sessionTimeoutWarningHappensInMsec >= this.sessionTimeoutInMsec; },
-			msg: function () { return this.ERRORS.WARNINGSMALLERTHANTIMEOUT; }
+			isValid: function () {
+				return this.sessionTimeoutWarningHappensInMsec
+						>= this.sessionTimeoutInMsec;
+			},
+			msg: function () {
+				return this.ERRORS.WARNINGSMALLERTHANTIMEOUT;
+			}
 		}
 	];
 
-	sessionTimer.factory = function (warningSecs, expiringSecs) {
-		this.count = 1;
-		this.pollTimeInMsec = 50;	// 0.1 sec
-		this.sessionTimeoutInMsec = expiringSecs * 1000;	// 20 mins - 3 secs
-		this.sessionTimeoutWarningHappensInMsec = warningSecs * 1000;	// 20 mins - 23 secs
-		this.countdownStarted = null;
-		this.sessionHasTimedout = false;
-		this.isResettingSession = false;
+	sessionTimer = function () {
+		var obj = global.Object.create(prototype);
+		obj.assignProperties.apply(obj, Array.prototype.slice.call(arguments));
+		return obj;
 	};
-	sessionTimer.factory.prototype = sessionTimer.prototype;
 
 	// ********************************************
 	// expose sessionTimer to global scope
